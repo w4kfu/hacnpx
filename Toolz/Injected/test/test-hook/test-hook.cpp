65155cb133a931bd9f75f4bbcc32b234af352b1d
w4kfu <gw4kfu@gmail.com>
2015-09-04 15:29:01 +0200

    update test hook

--- Toolz/Injected/test/test-hook/test-hook.cpp
+++ Toolz/Injected/test/test-hook/test-hook.cpp
@@ -2,7 +2,12 @@
 
 VOID HookMessageBoxA(PPUSHED_REGS pRegs)
 {
-    DbgMsg("[+] MessageBoxA! called from "HEX_FORMAT"\n", GET_RETURN_ADDR(pRegs));
+    DbgMsg("[+] HookMessageBoxA - MessageBoxA! called from "HEX_FORMAT"\n", GET_RETURN_ADDR(pRegs));
+}
+
+VOID HookInlineMesssageBoxA(PPUSHED_REGS pRegs)
+{
+    DbgMsg("[+] HookInlineMesssageBoxA - MessageBoxA! called from "HEX_FORMAT"\n", GET_RETURN_ADDR(pRegs));
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
@@ -15,6 +20,9 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
         LoadLibraryA("USER32.dll");
         if (SetupIATHook((ULONG_PTR)GetModuleHandleA(NULL), "USER32.dll", "MessageBoxA", (PROC)HookMessageBoxA) == FALSE) {
             DbgMsg("[-] SetupIATHook failed!\n");
+        }
+        if (SetupInlineHook("USER32.dll", "MessageBoxA", (PROC)HookInlineMesssageBoxA) == FALSE) {
+            DbgMsg("[-] SetupInlineHook failed!\n");
         }
     }
     return TRUE;
