b06e669872373990428d79e2aa233838ad3890ac
w4kfu <gw4kfu@gmail.com>
2015-09-16 19:55:20 +0200

    add ini file config to injector

--- Toolz/Injector/injector.c
+++ Toolz/Injector/injector.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <Windows.h>
 
-void InjectCreateProcess(PCHAR pName, PCHAR pDllName)
+VOID InjectCreateProcess(PCHAR pName, PCHAR pDllName, PCHAR pCmdArg)
 {
     STARTUPINFOA si;
     PROCESS_INFORMATION pi;
     ULONG_PTR dwAddr;
     HANDLE hThread;
     HMODULE hKernel32;
+    CHAR CurrentPath[MAX_PATH ];
 
+    if (!GetCurrentDirectory(sizeof(CurrentPath) - 1, CurrentPath)) {
+        printf("[-] InjectCreateProcess - GetCurrentDirectory failed : %lu\n", GetLastError());
+        return;
+    }
     hKernel32 = GetModuleHandleA("kernel32.dll");
     memset(&si, 0, sizeof(STARTUPINFO));
     si.cb = sizeof(STARTUPINFO);
     memset(&pi, 0, sizeof(PROCESS_INFORMATION));
-    if (!CreateProcessA(pName, NULL, 0, 0, 0, CREATE_SUSPENDED, 0, 0, &si, &pi)) {
+    if (!CreateProcessA(pName, pCmdArg, NULL, NULL, FALSE, CREATE_SUSPENDED, GetEnvironmentStrings(), CurrentPath, &si, &pi)) {
             printf("[-] InjectCreateProcess - CreateProcessA() failed : %lu\n", GetLastError());
             exit(EXIT_FAILURE);
     }
@@ -32,12 +37,56 @@ void InjectCreateProcess(PCHAR pName, PCHAR pDllName)
     CloseHandle(hThread);
 }
 
+BOOL CheckIniFile(PCHAR OriginalExeName, PCHAR DllName)
+{
+    CHAR CurrentPath[MAX_PATH];
+    CHAR FilePath[MAX_PATH];
+    
+    if (!GetCurrentDirectory(sizeof(CurrentPath) - 1, CurrentPath)) {
+        printf("[-] CheckIniFile - GetCurrentDirectory failed : %lu\n", GetLastError());
+        return FALSE;
+    }
+    sprintf_s(FilePath, sizeof (FilePath) - 1, "%s\\injector.ini", CurrentPath);
+    if (GetPrivateProfileStringA("injector", "original_executable_name", NULL, OriginalExeName, MAX_PATH, FilePath) == 0) {
+        printf("[-] CheckIniFile - GetPrivateProfileString failed : %lu\n", GetLastError());
+        return FALSE;
+    }
+    if (GetPrivateProfileStringA("injector", "dll_name", NULL, DllName, MAX_PATH, FilePath) == 0) {
+        printf("[-] CheckIniFile - GetPrivateProfileString failed : %lu\n", GetLastError());
+        return FALSE;
+    }
+    return TRUE;
+}
+
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        printf("Usage : %s <target.exe> <dll_name.dll>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    CHAR OriginalExeName[MAX_PATH];
+    CHAR DllName[MAX_PATH];
+    PCHAR CmdLine = NULL;
+    CHAR FullCmdLine[MAX_PATH];
+    
+    if (CheckIniFile((PCHAR)&OriginalExeName, (PCHAR)&DllName) == TRUE) {
+        printf("[+] Injecting %s into %s process\n", DllName, OriginalExeName);
+        CmdLine = GetCommandLine();
+        if (CmdLine) {
+            CmdLine = strchr(CmdLine, ' ');
+            if (CmdLine) {
+                CmdLine += 2;
+                sprintf_s(FullCmdLine, MAX_PATH - 1, "%s %s", OriginalExeName, CmdLine);
+            }
+            else {
+                sprintf_s(FullCmdLine, MAX_PATH - 1, "%s", OriginalExeName);
+            }
+        }
+        printf("[+] FullCmdLine : %s\n", FullCmdLine);
+        InjectCreateProcess(OriginalExeName, DllName, FullCmdLine);
     }
-    InjectCreateProcess(argv[1], argv[2]);
+    else {
+        if (argc < 3) {
+            printf("Usage : %s <target.exe> <dll_name.dll>\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        InjectCreateProcess(argv[1], argv[2], argv[1]);
+    }
     return (0);
 }
