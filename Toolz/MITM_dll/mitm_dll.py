3c30fec5d04d66f6817ce9ff353c5b25191befd5
Samuel CHEVET <gw4kfu@gmail.com>
2017-03-07 15:37:54 -0500

    update mitm

--- Toolz/MITM_dll/mitm_dll.py
+++ Toolz/MITM_dll/mitm_dll.py
@@ -1,59 +1,68 @@
 import sys
 import os
 import shutil
-from elfesteem import *
+#from elfesteem import *
+import pefile
 
-def create_makefile(res_dir, makefile_name, fake_c_name, fake_asm_name, fake_dll):
+def create_makefile(res_dir, makefile_name, fake_c_name, fake_asm_name, fake_dll, pe_filename):
     makefile = '''@cl.exe '''+fake_c_name+''' /W3 /GF /GS- /GA /MT /nologo /c /TC /DTRAMPO
 @ml64.exe /DTRAMPO /c '''+fake_asm_name+'''
-@link '''+os.path.splitext(fake_c_name)[0]+'''.obj '''+os.path.splitext(fake_asm_name)[0]+'''.obj /dll /release /subsystem:console /out:'''+fake_dll+''' /MACHINE:X64 /MANIFEST:NO /merge:.rdata=.text
+@link '''+os.path.splitext(fake_c_name)[0]+'''.obj '''+os.path.splitext(fake_asm_name)[0]+'''.obj /dll /release /subsystem:console /out:'''+fake_dll+''' /MACHINE:X64 /MANIFEST:NO /merge:.rdata=.text /def:'''+ os.path.splitext(pe_filename)[0] +'''.def
 del *.obj
 del *.exp
 '''
     open(res_dir + makefile_name, "wb").write(makefile)
 
+def create_fake_def(l_export, res_dir, fake_asm_name, pe_filename):
+    fname = os.path.splitext(pe_filename)[0] + ".def"
+    fd_out = open(res_dir + fname, "w")
+    fd_out.write("EXPORTS\n")
+    for name, virt in l_export:
+        fd_out.write("%s=%s\n" % (name, os.path.splitext(fake_asm_name)[0]+"_"+name))
+    
+    
 def create_fake_asm(l_export, res_dir, fake_asm_name):
     asm_head = '''.data
 
 save_return_addr    dq 0
-save_rcx	    dq 0
+save_rcx            dq 0
 
 .code
 
 PUSHAQ MACRO 
-	push rax		; + 0x08
-	push rbx		; + 0x10
-	push rcx		; + 0x18
-	push rdx		; + 0x20
-	push rbp		; + 0x28
-	push rdi		; + 0x30
-	push rsi		; + 0x40
-	push r8 		; + 0x48
-	push r9 		; + 0x50
-	push r10		; + 0x58
-	push r11		; + 0x60
-	push r12		; + 0x68
-	push r13		; + 0x70
-	push r14		; + 0x78
-	push r15		; + 0x80
+    push rax        ; + 0x08
+    push rbx        ; + 0x10
+    push rcx        ; + 0x18
+    push rdx        ; + 0x20
+    push rbp        ; + 0x28
+    push rdi        ; + 0x30
+    push rsi        ; + 0x40
+    push r8         ; + 0x48
+    push r9         ; + 0x50
+    push r10        ; + 0x58
+    push r11        ; + 0x60
+    push r12        ; + 0x68
+    push r13        ; + 0x70
+    push r14        ; + 0x78
+    push r15        ; + 0x80
 ENDM
 
 POPAQ MACRO 
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-	pop r11
-	pop r10
-	pop r9 
-	pop r8 
-	pop rsi
-	pop rdi
-	pop rbp
-	pop rdx
-	pop rcx
-	pop rbx
-	pop rax
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9 
+    pop r8 
+    pop rsi
+    pop rdi
+    pop rbp
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
 ENDM
 
 '''
@@ -63,43 +72,44 @@ ENDM
         fd_out.write("EXTERN Before_%s:PROC\nEXTERN After_%s:PROC\nIFDEF TRAMPO\nEXTERN Real_%s:QWORD\nENDIF\n" % (name, name, name))
     fd_out.write("\n\n")
     for name, virt in l_export:
-        buf = name+''' PROC EXPORT
-
+        #buf = os.path.splitext(fake_asm_name)[0]+"_"+name+''' PROC EXPORT
+        buf = os.path.splitext(fake_asm_name)[0]+"_"+name+''' PROC
+        
     IFDEF TRAMPO
 
     PUSHAQ
-    mov	rcx, rsp
-    sub rsp, 20h
-    call Before_'''+name+'''
-    add rsp, 20h
+    mov     rcx, rsp
+    sub     rsp, 20h
+    call    Before_'''+name+'''
+    add     rsp, 20h
     POPAQ
     
-    mov	rax, [rsp]
-    mov	save_return_addr, rax
-    add	rsp, 8
-    call qword ptr [Real_'''+name+''']
-    sub	rsp, 8
-    mov	[rsp - 8], rax
-    mov	rax, save_return_addr
-    mov	[rsp], rax
-    mov	rax, [rsp - 8]
+    mov     rax, [rsp]
+    mov     save_return_addr, rax
+    add     rsp, 8
+    call    qword ptr [Real_'''+name+''']
+    sub     rsp, 8
+    mov     [rsp - 8], rax
+    mov     rax, save_return_addr
+    mov     [rsp], rax
+    mov     rax, [rsp - 8]
 
     PUSHAQ
-    mov	rcx, rsp
-    sub rsp, 20h
-    call After_'''+name+'''
-    add rsp, 20h
+    mov     rcx, rsp
+    sub     rsp, 20h
+    call    After_'''+name+'''
+    add     rsp, 20h
     POPAQ
 
     ELSE
     
-    xor rax, rax
+    xor     rax, rax
 
     ENDIF
     
     ret
     
-'''+name+''' ENDP\n\n'''
+'''+os.path.splitext(fake_asm_name)[0]+"_"+name+''' ENDP\n\n'''
         fd_out.write(buf)
     fd_out.write("\n\nend\n")
     fd_out.close()
@@ -224,21 +234,21 @@ VOID LoadRealExport(VOID)
 {
         HMODULE hModuleOriginal;
 
-	hModuleOriginal = LoadLibraryA(ORIGINAL_DLL_NAME);
-	if (hModuleOriginal == NULL)
-	{
-		dbg_msg("[-] LoadLibraryA(%s) failed : %u\\n", ORIGINAL_DLL_NAME, GetLastError());
-		return;
-	}
-	dbg_msg("[+] Original DLL : 0x%016llX\\n", hModuleOriginal);
-	LoadAllRealExport(hModuleOriginal);
+    hModuleOriginal = LoadLibraryA(ORIGINAL_DLL_NAME);
+    if (hModuleOriginal == NULL)
+    {
+        dbg_msg("[-] LoadLibraryA(%s) failed : %u\\n", ORIGINAL_DLL_NAME, GetLastError());
+        return;
+    }
+    dbg_msg("[+] Original DLL : 0x%016llX\\n", hModuleOriginal);
+    LoadAllRealExport(hModuleOriginal);
 }
 
 #endif
 
 VOID PrintInfoReg(struct all_reg_64 *reg, BOOL Head)
 {
-	MEMORY_BASIC_INFORMATION64 mbi = {0};
+    MEMORY_BASIC_INFORMATION64 mbi = {0};
 
     if (Head == TRUE)
     {
@@ -279,32 +289,32 @@ VOID PrintBottom(VOID)
 
 VOID MakeConsole(VOID)
 {
-	FILE *stream;
-
-	AllocConsole();
-	freopen_s(&stream, "CONIN$", "rb", stdin);
-	freopen_s(&stream, "CONOUT$", "wb", stdout);
-	freopen_s(&stream, "CONOUT$", "wb", stderr);
+    FILE *stream;
+    
+    AllocConsole();
+    freopen_s(&stream, "CONIN$", "rb", stdin);
+    freopen_s(&stream, "CONOUT$", "wb", stdout);
+    freopen_s(&stream, "CONOUT$", "wb", stderr);
 }
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
-	HMODULE hModule;
-
-	if (fdwReason == DLL_PROCESS_ATTACH)
-	{
-		MakeConsole();
-		hModule = GetModuleHandleA(NULL);
-		DisableThreadLibraryCalls(hModule);
-		dbg_msg("[+] hModule = 0x%016llX\\n", hModule);
-		
-		#if TRAMPO
-		
-		LoadRealExport();
-		
-		#endif
-	}
-	return TRUE;
+    HMODULE hModule;
+    
+    if (fdwReason == DLL_PROCESS_ATTACH)
+    {
+        MakeConsole();
+        hModule = GetModuleHandleA(NULL);
+        DisableThreadLibraryCalls(hModule);
+        dbg_msg("[+] hModule = 0x%016llX\\n", hModule);
+        
+        #if TRAMPO
+        
+        LoadRealExport();
+        
+        #endif
+    }
+    return TRUE;
 }
 '''
     open(res_dir + fake_c_name, "wb").write(fake_c)
@@ -336,10 +346,14 @@ def createdir(dirname):
 
 def get_export_name_addr_list(e):
     out = []
-    for i, n in enumerate(e.DirExport.f_names):
-        addr = e.DirExport.f_address[e.DirExport.f_nameordinals[i].ordinal]
-        f_name = n.name.name
-        out.append((f_name, e.rva2virt(addr.rva)))
+    for export in e.DIRECTORY_ENTRY_EXPORT.symbols:
+        if export.address is not None:
+            name = "None"
+            if not export.name:
+                raise ValueError("[-] meh :(")
+            else:
+                name = export.name
+        out.append((name, export.address))
     return out
 
 if __name__ == '__main__':        
@@ -347,21 +361,22 @@ if __name__ == '__main__':
         print "[-] Usage: mitm_dll.py <DLL>\n"
         sys.exit()
     else:
-        filename = sys.argv[1]
-        e = pe_init.PE(open(filename, 'rb').read())
-        l_export = get_export_name_addr_list(e)
+        pe_filename = sys.argv[1]
+        pe = pefile.PE(pe_filename)
+        l_export = get_export_name_addr_list(pe)
         for name, virt in l_export:
             print name
-        filename = os.path.splitext(filename)[0]
+        pe_filename = os.path.splitext(pe_filename)[0]
         makefile_name = "make.bat"
-        res_dir = filename + "_res_dir/"
-        fake_c_name = filename + ".c"
-        fake_asm_name = filename + "_trampo.asm"
-        fake_dll = filename + ".dll"
-        original_dll = filename + "_original.dll"
+        res_dir = pe_filename + "_res_dir/"
+        fake_c_name = pe_filename + ".c"
+        fake_asm_name = pe_filename + "_trampo.asm"
+        fake_dll = pe_filename + ".dll"
+        original_dll = pe_filename + "_original.dll"
         createdir(res_dir)
-        create_makefile(res_dir, makefile_name, fake_c_name, fake_asm_name, fake_dll)
+        create_makefile(res_dir, makefile_name, fake_c_name, fake_asm_name, fake_dll, pe_filename)
         create_fake_asm(l_export, res_dir, fake_asm_name)
         shutil.copyfile(sys.argv[1], res_dir + original_dll)
         create_fake_c(l_export, res_dir, fake_c_name, original_dll)
         append_fake_export(l_export, res_dir, fake_c_name)
+        create_fake_def(l_export, res_dir, fake_asm_name, pe_filename)
